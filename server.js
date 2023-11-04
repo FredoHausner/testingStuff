eca9914432fdcf1f0d7e2e2f2f3b62388e361da0
Fredo Gonzalez <FredoHausner@gmail.com>
2023-11-04 17:33:06 -0600

    Commit

--- server.js
+++ server.js
@@ -21,10 +21,34 @@ app.use("/admin", express.static(path.join(__dirname, "web/admin/")));
 
 // app.use("/admin", express.static(path.join(__dirname, "admin/www")));
 
-app.get("*", (req, res) => {
-  res.send("404");
+// app.get("*", (req, res) => {
+//   res.send("404");
+// });
+
+// app.use("/user", (req, res) => {
+//   console.log(req.method);
+//   if (req.method === "GET") {
+//     res.send("GET USER");
+//   } else if (req.method === "POST") {
+//     res.send("POST USER");
+//   } else if (req.method === "DELETE") {
+//     res.send("DELETE USER");
+//   }
+// });
+app.get("/user", (req, res) => {
+  console.log(req.method);
+  res.send("GET USER");
+});
+app.post("/user", (req, res) => {
+  console.log(req.method);
+  res.send("POST USER");
+});
+app.delete("/user", (req, res) => {
+  console.log(req.method);
+  res.send("DELETE USER");
 });
 
+app.use("/*", express.static(path.join(__dirname, "web/404/")));
 app.listen(PORT, () => {
   console.log(`server listening on port: ${PORT}`);
 });
